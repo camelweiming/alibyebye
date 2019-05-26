051d9b40664c6b638e1cbf60c13cc1df578442e9
cenpeng.lwm <cenpeng.lwm@alibaba-inc.com>
2019-05-26 15:51:02 +0800

    增加流程引擎

--- bye-biz/src/main/java/com/abb/bye/utils/LoginUtil.java
+++ bye-biz/src/main/java/com/abb/bye/utils/LoginUtil.java
@@ -1,9 +1,17 @@
 package com.abb.bye.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.ReflectionUtils;
+
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
 import java.security.InvalidKeyException;
 
 /**
@@ -11,7 +19,11 @@ import java.security.InvalidKeyException;
  * @since 2019/5/26
  */
 public class LoginUtil {
-    private static String clientKey = "test";
+    private static Logger logger = LoggerFactory.getLogger(LoginUtil.class);
+    private static String clientKey = "bye";
+    private static String loginCookieName = "__login_user__";
+    private static Boolean useSecureCookie = false;
+    private static Method setHttpOnlyMethod;
     public static final String ENCODING_UTF8 = "UTF-8";
     private static Cipher clientCookieEncryptCipher;
     private static Cipher clientCookieDecryptCipher;
@@ -19,6 +31,7 @@ public class LoginUtil {
     static {
         clientCookieEncryptCipher = EncodeUtil.getCipher(clientKey, true);
         clientCookieDecryptCipher = EncodeUtil.getCipher(clientKey, false);
+        setHttpOnlyMethod = ReflectionUtils.findMethod(Cookie.class, "setHttpOnly", boolean.class);
     }
 
     public static String encodeCookie(String value) throws InvalidKeyException, IllegalBlockSizeException,
@@ -29,6 +42,49 @@ public class LoginUtil {
     public static String decodeCookie(String value) throws InvalidKeyException, IllegalBlockSizeException,
         BadPaddingException, UnsupportedEncodingException {
         return EncodeUtil.decodeText(clientCookieDecryptCipher, value, ENCODING_UTF8);
+    }
+
+    private static String getCookiePath(HttpServletRequest request) {
+        String contextPath = request.getContextPath();
+        return contextPath.length() > 0 ? contextPath : "/";
+    }
+
+    public static String getLoginUserName(HttpServletRequest request) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException, UnsupportedEncodingException {
+        Cookie[] cookies = request.getCookies();
+        if ((cookies == null) || (cookies.length == 0)) {
+            return null;
+        }
+        for (Cookie cookie : cookies) {
+            if (loginCookieName.equals(cookie.getName())) {
+                return decodeCookie(cookie.getValue());
+            }
+        }
+        return null;
+    }
+
+    public static void setLoginCookie(String domain, String value, int maxAge, HttpServletRequest request, HttpServletResponse response)
+        throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException, UnsupportedEncodingException {
+        String cookieValue = encodeCookie(value);
+        Cookie cookie = new Cookie(loginCookieName, cookieValue);
+        cookie.setMaxAge(maxAge);
+        cookie.setPath(getCookiePath(request));
+        if (domain != null) {
+            cookie.setDomain(domain);
+        }
+        if (maxAge < 1) {
+            cookie.setVersion(1);
+        }
+        if (useSecureCookie == null) {
+            cookie.setSecure(request.isSecure());
+        } else {
+            cookie.setSecure(useSecureCookie);
+        }
+        if (setHttpOnlyMethod != null) {
+            ReflectionUtils.invokeMethod(setHttpOnlyMethod, cookie, Boolean.TRUE);
+        } else if (logger.isDebugEnabled()) {
+            logger.debug("Note: Cookie will not be marked as HttpOnly because you are not using Servlet 3.0 (Cookie#setHttpOnly(boolean) was not found).");
+        }
+        response.addCookie(cookie);
     }
 
     public static void main(String[] args) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException, UnsupportedEncodingException {
--- bye-web/src/main/java/com/abb/bye/web/UserController.java
+++ bye-web/src/main/java/com/abb/bye/web/UserController.java
@@ -2,7 +2,10 @@ package com.abb.bye.web;
 
 import com.abb.bye.client.domain.ResultDTO;
 import com.abb.bye.client.domain.UserAuthorityDTO;
+import com.abb.bye.client.domain.UserDTO;
 import com.abb.bye.client.service.UserAuthorityService;
+import com.abb.bye.client.service.UserService;
+import com.abb.bye.utils.LoginUtil;
 import com.alibaba.boot.velocity.annotation.VelocityLayout;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -12,8 +15,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author cenpeng.lwm
@@ -24,16 +30,18 @@ public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(AppController.class);
     @Resource
     private UserAuthorityService userAuthorityService;
+    @Resource
+    private UserService userService;
 
     @RequestMapping(value = "sign_in.htm", method = {RequestMethod.POST, RequestMethod.GET})
     @VelocityLayout("/velocity/layout/layout_login.vm")
-    String signIn(Model model, @RequestParam(required = false) String name, @RequestParam(required = false) String password) {
+    ModelAndView signIn(Model model, @RequestParam(required = false) String name, @RequestParam(required = false) String password, HttpServletRequest request, HttpServletResponse response) {
         String vm = "sign_in";
         try {
             model.addAttribute("name", name);
             model.addAttribute("password", password);
             if (StringUtils.isBlank(name)) {
-                return vm;
+                return new ModelAndView(vm);
             }
             if (StringUtils.isBlank(password)) {
                 model.addAttribute("errorMsg", "密码不能为空");
@@ -45,11 +53,15 @@ public class UserController {
             logger.info("register:" + userAuthorityDTO.getName() + " res:" + res);
             if (!res.isSuccess()) {
                 model.addAttribute("errorMsg", res.getErrMsg());
+                return new ModelAndView(vm);
             }
-            return vm;
+            UserDTO user = userService.getById(res.getData()).getData();
+            LoginUtil.setLoginCookie(null, user.getUserName(), 3600, request, response);
+            return new ModelAndView("redirect:/");
         } catch (Throwable e) {
             logger.error("Error signIn", e);
-            throw e;
+            model.addAttribute("errorMsg", "system error");
+            return new ModelAndView(vm);
         }
     }
 }
